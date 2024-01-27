ceca8036ab2c4b53d0251138ab59a80e70328d00
EudoraYeow <S10242444@connect.np.edu.sg>
2024-01-27 11:03:29 +0800

    Added a home controller and index.cshtml

---
+++ InsightOpticPro_PFD_Final/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsightOpticPro_PFD_Final.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
